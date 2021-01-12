419a1ef1c8de7d07ce417870869deb0c2a6617da
Max23Esau <esa-max23@live.com>
2021-01-11 23:20:57 -0600

    add ajax2.js

---
+++ ajax2.js
@@ -0,0 +1,37 @@
+// console.log('ok');
+document.querySelector('#boton').addEventListener('click', traerDatos);
+
+function traerDatos() {
+  // console.log('Dentro de la funcion');
+
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.open('GET', 'data.json', true);
+
+  xhttp.send();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // console.log(this.responseText);
+
+      let datos = JSON.parse(this.responseText);
+      // console.log(datos);
+      let respuesta = document.querySelector('#respuesta');
+      respuesta.innerHTML = '';
+
+      for (let item of datos) {
+        // console.log(item.artista);
+        respuesta.innerHTML += `
+        <tr>
+          <td>${item.titulo}</td>
+          <td>${item.artista}</td>
+          <td>${item.pais}</td>
+          <td>${item.compañia}</td>
+          <td>${item.precio}</td>
+          <td>${item.año}</td>
+        </tr>
+        `;
+      }
+    }
+  };
+}
