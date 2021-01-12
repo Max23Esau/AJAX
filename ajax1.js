bcc7d0bc828123ce61878674c957ed2f78c25daf
Max23Esau <esa-max23@live.com>
2021-01-11 19:51:44 -0600

    first commit. Working with AJAX

---
+++ ajax1.js
@@ -0,0 +1,16 @@
+document.querySelector('#boton'), addEventListener('click', traerDatos);
+
+function traerDatos() {
+  // console.log('No me toques');
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('GET', 'archivo.txt', true);
+
+  xhttp.send();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // console.log(this.responseText)
+      document.querySelector('#respuesta').innerHTML = this.responseText;
+    }
+  };
+}
